7b30ee40fcae8ca748c237df9a87f8caf1585cd3
Anton Mitsev <Anton_Mitsev@epam.com>
2018-07-13 17:23:05 +0300

    [00001] feature: REST CRUD boilerplate
    Taken express CRUD
    
    https://medium.freecodecamp.org/building-a-simple-node-js-api-in-under-30-minutes-a07ea9e390d2

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const defaults       = require('./config/defaults');
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+
+const app            = express();
+
+const port = 8000;
+
+//Mega important :)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    // Make sure you add the database name and not the collection name
+    let db = database.db("note-api")
+    require('./app/routes')(app, db);
+  
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
+    });               
+  });
+
+
+
