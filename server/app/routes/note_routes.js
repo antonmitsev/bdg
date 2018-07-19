df493fcdf92fb59400f1c38ae6e90c76ae7d8d76
Anton Mitsev <Anton_Mitsev@epam.com>
2018-07-19 17:48:37 +0300

    [BDG-00002] FE-BE communication
    Angular url-encoded params create
    BE Server - multi-API, multi collection
    
    -

--- server/app/routes/note_routes.js
+++ server/app/routes/note_routes.js
@@ -1,22 +1,60 @@
 const defaults       = require('../../config/defaults');
 var ObjectID = require('mongodb').ObjectID;
 
+Object.prototype.getAllowedFields = function(fields) {
+    let allowedFields = { };
+    for(prop in fields) {
+        if(fields.hasOwnProperty(prop)) {
+            let newObj;
+            if(this.hasOwnProperty(prop)){
+                newObj = JSON.parse(JSON.stringify(this[prop]));
+            }else{
+                newObj = null;
+            }
+            if(! fields[prop]){
+                allowedFields[prop] = newObj;
+            }else{
+                allowedFields[fields[prop]] = newObj;
+            }
+        }
+    }
+    return allowedFields;
+}
+
 module.exports = function(app, db) {
-    var api = '';
-    app.use(function(req, res, next){
-        api = req.url.trim().replace(/\/+$/, '');
-        console.log(api);
+    var api = '', 
+        dbName = '',
+        fields = {},
+        noteToAdd = null
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer");
         next();
     });
 
-    app.get('/notes', (req, res) => {
+    app.use('/api/:api/:id?', function(req, res, next){
+        api = req.params.api;
+        if(defaults.apis[api]){
+            dbName = defaults.apis[api].db;
+            fields = defaults.apis[api].fields;
+            noteToAdd = req.body.getAllowedFields(fields);
+            console.log(noteToAdd, req.body);
+            next();
+        }else{
+            res.send({'error':'Api not found: ' + api + '!'});
+            res.end();
+        }
+    });
+
+    app.get('/api/:api', (req, res) => {
         let page = parseInt('0' + req.query.page);
         let records = parseInt('0' + req.query.records);
         if(records <= 0){
             records = defaults.records;
         }
         
-        db.collection('notes').find().skip(page * records).limit(records).toArray((err, items) => {
+        db.collection(dbName).find().skip(page * records).limit(records).toArray((err, items) => {
             if (err) {
                 res.send({'error':'An error has occurred'});
             } else {
@@ -26,10 +64,10 @@ module.exports = function(app, db) {
     });
 
 
-    app.get('/notes/:id', (req, res) => {
+    app.get('/api/:api/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
-            db.collection('notes').findOne(details, (err, item) => {
+            db.collection(dbName).findOne(details, (err, item) => {
             if (err) {
                 res.send({'error':'An error has occurred'});
             } else {
@@ -38,30 +76,30 @@ module.exports = function(app, db) {
         });
     });
 
-    app.delete('/notes/:id', (req, res) => {
+    app.delete('/api/:api/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
-        db.collection('notes').remove(details, (err, item) => {
+        db.collection(dbName).remove(details, (err, item) => {
             if (err) {
-            res.send({'error':'An error has occurred'});
+                res.send({'error':'An error has occurred'});
             } else {
-            res.send('Note ' + id + ' deleted!');
+                res.send('Note ' + id + ' deleted!');
             } 
         });
     });
       
-    app.post('/notes', (req, res) => {
-        const note = { text: req.body.body, title: req.body.title };
-        db.collection('notes').insert(note, (err, result) => {
-        if (err) { 
-            res.send({ 'error': 'An error has occurred' }); 
-        } else {
-            res.send(result.ops[0]);
-        }
+    app.post('/api/:api', (req, res) => {
+
+        db.collection(dbName).insert(noteToAdd, (err, result) => {
+            if (err) { 
+                res.send({ 'error': 'An error has occurred' }); 
+            } else {
+                res.send(result.ops[0]);
+            }
         });
     });
 
-    app.put('/notes/:id', (req, res) => {
+    app.put('/api/:api/:id', (req, res) => {
         const id = req.params.id;
         const details = { '_id': new ObjectID(id) };
         const note = { text: req.body.body, title: req.body.title };
@@ -72,6 +110,8 @@ module.exports = function(app, db) {
               res.send(note);
           } 
         });
-      });    
+      });  
+      
+    
 
 };
